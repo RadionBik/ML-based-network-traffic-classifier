b63c37bddec867d0bce72da6e0b9ce2a545d4dca
Radion Bikmukhamedov <radion.bikmukhamedov@gmail.com>
2019-08-13 00:00:29 +0300

    fix broken reporting and minor code prettifying

--- classifiers.py
+++ classifiers.py
@@ -17,12 +17,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-from config_loader import Config_Init
+from config_loader import ConfigInit
 from feature_processing import read_csv, FeatureTransformer
 from report import ClassifierEvaluator
 
-
-logger = logging.getLogger(__name__)
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger()
 
 
 class TrafficClassifiers:
@@ -33,8 +34,7 @@ class TrafficClassifiers:
             'LogRegr': {"C": [10, 100, 1000],
                       #"tol": [0.00001,0.0001,0.001],
                       #"max_features": sp_randint(1, 11),
-                    },
-
+                      },
                       #{'alpha': [0.1, 0.01, 0.001, 0.0001]},
 
             'SVM': {'estimator__C': [0.1, 1, 10],
@@ -51,55 +51,50 @@ class TrafficClassifiers:
                     #{'kernel': ['linear'], 'C': [1, 10, 100, 1000]},
                     #],
 
-            'DecTree': {"max_depth": [i for i in range(5,20) if i%3==0],
-                      "max_features": [i for i in range(10,40) if i%10==0],
-                      "criterion": ["entropy"]},
-            'RandomForest':{"n_estimators":[i for i in range(10,50) if i%10==0],
-                      "max_depth": [i for i in range(3,16) if i%3==0],
-                      #"max_features": sp_randint(1, 11),
-                      "criterion": ["entropy"]},
-            'GradBoost':{"n_estimators" : [50],
-                      "max_depth":  [i for i in range(2,6)],
-                      #"max_features": sp_randint(1, 11),
-                      "learning_rate": [0.01,0.05,0.1]},
-            'MLP':{"hidden_layer_sizes":[(i,i) for i in range(80,121) if i%40==0],
-                   "alpha":[0.0001,0.001,0.01],}
+            'DecTree': {"max_depth": [i for i in range(5, 20) if i % 3 == 0],
+                        "max_features": [i for i in range(10, 40) if i % 10 == 0],
+                        "criterion": ["entropy"]},
+            'RandomForest': {"n_estimators": [i for i in range(10, 50) if i % 10 == 0],
+                             "max_depth": [i for i in range(3, 16) if i % 3 == 0],
+                             # "max_features": sp_randint(1, 11),
+                             "criterion": ["entropy"]},
+            'GradBoost': {"n_estimators" : [50],
+                          "max_depth":  [i for i in range(2,6)],
+                          # "max_features": sp_randint(1, 11),
+                          "learning_rate": [0.01, 0.05, 0.1]},
+            'MLP': {"hidden_layer_sizes": [(i, i) for i in range(80, 121) if i % 40 == 0],
+                    "alpha": [0.0001, 0.001, 0.01]}
         }
 
         self.classifiers = {
-            'LogRegr': LogisticRegression(random_state=self.random_seed,
-                                         multi_class='auto',
-                                         solver='lbfgs',
-                                         max_iter=200,
-                                         n_jobs=-1),
+            'LogRegr':   LogisticRegression(random_state=self.random_seed,
+                                            multi_class='auto',
+                                            solver='lbfgs',
+                                            max_iter=200,
+                                            n_jobs=-1),
             #SGDClassifier(loss='log', n_jobs=-1, random_state=self.random_seed,tol=1e-4),
 
             'SVM':
             #SGDClassifier(loss='hinge', n_jobs=-1, random_state=self.random_seed,tol=1e-4),
             #LinearSVC(random_state=self.random_seed, tol=1e-4, dual=True),
             #SVC(random_state=self.random_seed, cache_size=1000),
-
             OneVsOneClassifier(LinearSVC(random_state=self.random_seed, tol=1e-5), n_jobs=-1),
             'DecTree': DecisionTreeClassifier(random_state=self.random_seed),
             'RandomForest': RandomForestClassifier(random_state=self.random_seed),
             'GradBoost': GradientBoostingClassifier(random_state=self.random_seed),
             'MLP': MLPClassifier(random_state=self.random_seed, max_iter=300)
         }
-        
-        self._suffix_for_optimized = '_opt'
 
-        if file_suffix:
-            self._suffix = file_suffix
-        else:
-            self._suffix = self._config['general']['fileSaverSuffix']
+        self._suffix_for_optimized = '_opt'
+        self._suffix = file_suffix if file_suffix else self._config['general']['fileSaverSuffix']
 
     def _search_classif_parameters(self, classifier_name, X, y):
     
         X_tr, X_val, y_tr, y_val = train_test_split(X, y,
-                                                  shuffle=True,
-                                                  test_size=.1,
-                                                  stratify=y,
-                                                  random_state=self.random_seed)
+                                                    shuffle=True,
+                                                    test_size=.1,
+                                                    stratify=y,
+                                                    random_state=self.random_seed)
         search = GridSearchCV(self.classifiers[classifier_name],
                               param_grid=self.parameter_search_space[classifier_name],
                               n_jobs=-1,
@@ -109,8 +104,7 @@ class TrafficClassifiers:
         start = time()
         search.fit(X_val, y_val)
         logger.info('Search took {:.2f} seconds'.format(time() - start))
-        logger.info('Best parameters are {} with score {:.4f}'.format(search.best_params_,
-                                                                search.best_score_))
+        logger.info('Best parameters are {} with score {:.4f}'.format(search.best_params_, search.best_score_))
         
         rand_state_key = 'random_state'
         if isinstance(self.classifiers[classifier_name], OneVsOneClassifier):
@@ -130,8 +124,8 @@ class TrafficClassifiers:
                 logger.info('Started fitting {}...'.format(classif_name))
                 self.classifiers[classif_name].fit(X, y)
                 joblib.dump(self.classifiers[classif_name],
-                           self._config['general']['classifiers_folder']+os.sep\
-                            +classif_name+opt_suffix+self._suffix+'.cla')
+                            self._config['general']['classifiers_folder']+os.sep\
+                            + classif_name+opt_suffix+self._suffix+'.cla')
 
     def load(self):
         for classif_name in self.classifiers:
@@ -162,7 +156,7 @@ def main():
 
     args = parser.parse_args()
 
-    conf = Config_Init(args.config).get()
+    conf = ConfigInit(args.config).get()
 
     logger.info('Loading csv file..')
     config = configparser.ConfigParser()
