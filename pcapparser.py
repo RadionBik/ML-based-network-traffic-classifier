b5285114d44c19fc0842fc79b3072d596db2402a
Eugene Katsevman <eugene.katsevman@gmail.com>
2019-08-14 03:37:24 +0300

    kill the PCAPParser class

--- pcapparser.py
+++ pcapparser.py
@@ -2,13 +2,29 @@
 
 import argparse
 import configparser
+import logging
 import os
 import re
 import socket
 from subprocess import Popen, PIPE
+import typing
 
 import dpkt
 import pandas as pd
+
+
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
+
+
+class Endpoint(typing.NamedTuple):
+    address: str
+    port: int
+
+
+class Connection(typing.NamedTuple):
+    proto: str
+    peers: typing.FrozenSet[Endpoint]
 
 
 def ip_to_string(inet) -> str:
@@ -33,36 +49,36 @@ def ip4_from_string(ip: str) -> bytes:
     return bytes(map(int, ip.split('.')))
 
 
-def _extract_feature_stats(raw_df):
+def _extract_rawflow_features(df: pd.DataFrame) -> dict:
     stats = {}
 
-    client_bulks = raw_df[(raw_df['transp_payload'] > 0) &
-                          (raw_df['is_client'] == 1)
-                          ]['transp_payload']
+    client_bulks = df[(df['transp_payload'] > 0) &
+                      (df['is_client'] == 1)
+                      ]['transp_payload']
 
-    server_bulks = raw_df[(raw_df['transp_payload'] > 0) &
-                          (raw_df['is_client'] == 0)
-                          ]['transp_payload']
+    server_bulks = df[(df['transp_payload'] > 0) &
+                      (df['is_client'] == 0)
+                      ]['transp_payload']
 
-    client_packets = raw_df[raw_df['is_client'] == 0
-                            ]['ip_payload']
+    client_packets = df[df['is_client'] == 0
+                        ]['ip_payload']
 
-    server_packets = raw_df[raw_df['is_client'] == 0
-                            ]['ip_payload']
+    server_packets = df[df['is_client'] == 0
+                        ]['ip_payload']
 
     fault_avoider = (
         lambda values, index=0: values.iloc[index] if len(values) > index else 0)
 
     stats.update({
-        'proto': raw_df['proto'].iloc[0],
-        'subproto': raw_df['subproto'].iloc[0],
-        'is_tcp': raw_df['is_tcp'].iloc[0],
+        'proto': df['proto'].iloc[0],
+        'subproto': df['subproto'].iloc[0],
+        'is_tcp': df['is_tcp'].iloc[0],
 
-        'client_found_tcp_flags': sorted(set(raw_df[raw_df['is_client'] == 1]['tcp_flags'])),
-        'server_found_tcp_flags': sorted(set(raw_df[raw_df['is_client'] == 0]['tcp_flags'])),
+        'client_found_tcp_flags': sorted(set(df[df['is_client'] == 1]['tcp_flags'])),
+        'server_found_tcp_flags': sorted(set(df[df['is_client'] == 0]['tcp_flags'])),
 
-        'client_tcp_window_mean': raw_df[raw_df['is_client'] == 1]['tcp_win'].mean(),
-        'server_tcp_window_mean': raw_df[raw_df['is_client'] == 0]['tcp_win'].mean(),
+        'client_tcp_window_mean': df[df['is_client'] == 1]['tcp_win'].mean(),
+        'server_tcp_window_mean': df[df['is_client'] == 0]['tcp_win'].mean(),
 
         'client_bulk0': fault_avoider(client_bulks, 0),
         'client_bulk1': fault_avoider(client_bulks, 1),
@@ -112,19 +128,19 @@ def _extract_feature_stats(raw_df):
     })
 
     iat_client = pd.to_timedelta(pd.Series(
-        raw_df[raw_df['is_client'] == 1].index).diff().fillna('0')) / pd.offsets.Second(1)
-    iat_client.index = raw_df[raw_df['is_client'] == 1].index
+        df[df['is_client'] == 1].index).diff().fillna('0')) / pd.offsets.Second(1)
+    iat_client.index = df[df['is_client'] == 1].index
 
     iat_server = pd.to_timedelta(pd.Series(
-        raw_df[raw_df['is_client'] == 0].index).diff().fillna('0')) / pd.offsets.Second(1)
-    iat_server.index = raw_df[raw_df['is_client'] == 0].index
+        df[df['is_client'] == 0].index).diff().fillna('0')) / pd.offsets.Second(1)
+    iat_server.index = df[df['is_client'] == 0].index
 
-    raw_df['IAT'] = pd.concat([iat_server, iat_client])
+    df['IAT'] = pd.concat([iat_server, iat_client])
 
-    client_iats = raw_df[raw_df['is_client'] == 1
-                         ]['IAT']
-    server_iats = raw_df[raw_df['is_client'] == 0
-                         ]['IAT']
+    client_iats = df[df['is_client'] == 1
+                     ]['IAT']
+    server_iats = df[df['is_client'] == 0
+                     ]['IAT']
 
     stats.update({
         'client_iat_mean': client_iats.mean(),
@@ -141,7 +157,7 @@ def _extract_feature_stats(raw_df):
     return stats
 
 
-def _get_raw_flow_df(flow):
+def _raw_flow_to_sorted_dataframe(flow):
     df = pd.DataFrame(flow)
     df.set_index(pd.to_datetime(df['TS'], unit='s'), inplace=True)
     return df.drop(['TS'], axis=1)
@@ -152,157 +168,141 @@ def _pure_filename(full_path):
     return os.path.splitext(basename)[0]
 
 
-class PCAPParser:
-    def __init__(self, config, traffic_filename=None):
-        super().__init__()
-        self._config = config
-        self.traffic_filename = traffic_filename
-        self.strip = int(self._config['parser']['packetLimitPerFlow'])
-        self._apps = {}
-        self._flows = {}
-        self.flow_features = pd.DataFrame()
-        self.csv_filename = ''
+PROTOCOL = r'(UDP|TCP)'
+IP4 = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+PORT = r'(\d{1,5})'
+APPPROTO = r'\[proto: [\d+\.]*\d+\/(\w+\.?\w+)*\]'
 
-    def __repr__(self):
-        if self.flow_features.shape[0] == 0:
-            return '{}, strip={}, 0 flows with features so far.'.format(self.traffic_filename,
-                                                                        self.strip)
-        else:
-            return '{}, strip={}, {} flows with features such as:\n{}'.format(self.traffic_filename,
-                                                                              self.strip,
-                                                                              self.flow_features.shape[0],
-                                                                              self.flow_features.head())
 
-    def _get_flow_labels(self):
-        pipe = Popen(['./' + self._config['parser']['nDPIfilename'],
-                      '-i', self.traffic_filename, "-v2"], stdout=PIPE)
-        raw = pipe.communicate()[0].decode("utf-8")
-        reg = re.compile(
-            r'(UDP|TCP) (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}) <?->? (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5}) \[proto: [\d+\.]*\d+\/(\w+\.?\w+)*\]')
+def _parse_ndpi_output(raw: str) -> dict:
+    regex = f'{PROTOCOL} {IP4}:{PORT} <?->? {IP4}:{PORT} {APPPROTO}'
+    reg = re.compile(regex)
 
-        apps = {}
-        for captures in re.findall(reg, raw):
-            transp_proto, ip1, port1, ip2, port2, app_proto = captures
+    apps = {}
+    for captures in re.findall(reg, raw):
+        transp_proto, ip1, port1, ip2, port2, app_proto = captures
 
-            port1 = int(port1)
-            port2 = int(port2)
-            key = (transp_proto.lower(),
-                   frozenset(((ip1, port1), (ip2, port2))))
-            apps[key] = app_proto
-        return apps
+        port1 = int(port1)
+        port2 = int(port2)
+        connection = Connection(transp_proto.lower(),
+                                frozenset([Endpoint(ip1, port1), Endpoint(ip2, port2)]))
+        apps[connection] = app_proto
+    return apps
 
-    def _get_raw_flows(self):
-        flows = dict.fromkeys(self._apps.keys())
-        client_tuple = dict.fromkeys(self._apps.keys())
-        with open(self.traffic_filename, "rb") as pcap_file:
-            for ts, raw in dpkt.pcap.Reader(pcap_file):
-                eth = dpkt.ethernet.Ethernet(raw)
 
-                ip = eth.data
-                seg = ip.data
+def _get_flow_apps(ndpi: str, filename: str) -> dict:
+    pipe = Popen(['./' + ndpi,
+                  '-i', filename, "-v2"], stdout=PIPE)
+    raw = pipe.communicate()[0].decode("utf-8")
+    return _parse_ndpi_output(raw)
 
-                # check if the packet is IP, TCP, UDP
-                if not isinstance(ip, dpkt.ip.IP):
-                    continue
 
-                if isinstance(seg, dpkt.tcp.TCP):
-                    # 2 and 18 correspond to active SYN and SYN&ACK flags
-                    # if (seg.flags & dpkt.tcp.TH_SYN):
-                    #    print(seg.flags)
-                    transp_proto = "tcp"
+DEFAULT_FLOW_FEATURES = frozenset(
+    {
+        'is_client',
+        'TS',
+        'ip_payload',
+        'transp_payload',
+        'tcp_flags',
+        'tcp_win',
+        'proto',
+        'subproto',
+        'is_tcp',
+   }
+)
 
-                elif isinstance(seg, dpkt.udp.UDP):
-                    transp_proto = "udp"
-                else:
-                    continue
 
-                key = (transp_proto, frozenset(
-                    ((ip_to_string(ip.src), seg.sport),
-                     (ip_to_string(ip.dst), seg.dport))))
+def Flow():
+    """ Looks like a class but it isn't """
+    return {feature: [] for feature in DEFAULT_FLOW_FEATURES}
 
-                assert key in client_tuple
 
-                # if client tuple is empty, then no packets from the flow has been seen so far
-                if not client_tuple[key]:
-                    client_tuple[key] = (ip_to_string(ip.src), seg.sport)
-                    flows[key] = {feature: [] for feature in ['is_client',
-                                                              'TS',
-                                                              'ip_payload',
-                                                              'transp_payload',
-                                                              'tcp_flags',
-                                                              'tcp_win',
-                                                              'proto',
-                                                              'subproto',
-                                                              'is_tcp',
-                                                              ]}
+def _get_raw_flows(apps: dict, filename: str, max_packets_per_flow: typing.Optional[int] = None) -> dict:
+    """ transform packets to flows for each app """
+    flows = dict.fromkeys(apps)
+    client_tuple = dict.fromkeys(apps.keys())
+    with open(filename, "rb") as pcap_file:
+        for timestamp, raw in dpkt.pcap.Reader(pcap_file):
+            eth = dpkt.ethernet.Ethernet(raw)
+            ip = eth.data
 
-                if self.strip != 0 and (len(flows[key]['TS'])) > self.strip:
-                    continue
+            if not isinstance(ip, dpkt.ip.IP):
+                continue
+            seg = ip.data
+            if isinstance(seg, dpkt.tcp.TCP):
+                transp_proto = "tcp"
+            elif isinstance(seg, dpkt.udp.UDP):
+                transp_proto = "udp"
+            else:
+                continue
 
-                flows[key]['TS'].append(ts)
-                flows[key]['ip_payload'].append(len(ip.data))
-                flows[key]['transp_payload'].append(len(seg.data))
+            source = Endpoint(ip_to_string(ip.src), seg.sport)
+            destination = Endpoint(ip_to_string(ip.dst), seg.dport)
+            connection = Connection(transp_proto, frozenset([source, destination]))
 
-                if client_tuple[key] == (ip_to_string(ip.src), seg.sport):
-                    flows[key]['is_client'].append(True)
-                elif client_tuple[key] == (ip_to_string(ip.dst), seg.dport):
-                    flows[key]['is_client'].append(False)
-                else:
-                    raise ValueError
+            assert connection in client_tuple
 
-                if transp_proto == 'tcp':
-                    flows[key]['tcp_flags'].append(seg.flags)
-                    flows[key]['tcp_win'].append(seg.win)
-                    flows[key]['is_tcp'].append(True)
+            # if client tuple is empty, then no packets from the flow has been seen so far
+            if not client_tuple[connection]:
+                client_tuple[connection] = source
+                flows[connection] = Flow()
+            flow = flows[connection]
+            if max_packets_per_flow and (len(flow['TS'])) > max_packets_per_flow:
+                continue
 
-                else:
-                    flows[key]['tcp_flags'].append(0)
-                    flows[key]['tcp_win'].append(0)
-                    flows[key]['is_tcp'].append(False)
+            flow['TS'].append(timestamp)
+            flow['ip_payload'].append(len(ip.data))
+            flow['transp_payload'].append(len(seg.data))
 
-                app = self._apps[key].split('.')
-                if len(app) == 1:
-                    flows[key]['proto'].append(app[0])
-                    flows[key]['subproto'].append('')
-                else:
-                    flows[key]['proto'].append(app[0])
-                    flows[key]['subproto'].append(app[1])
+            if client_tuple[connection] == source:
+                flow['is_client'].append(True)
+            elif client_tuple[connection] == destination:
+                flow['is_client'].append(False)
+            else:
+                raise ValueError
 
-        return flows
+            if transp_proto == 'tcp':
+                flow['tcp_flags'].append(seg.flags)
+                flow['tcp_win'].append(seg.win)
+                flow['is_tcp'].append(True)
+            else:
+                flow['tcp_flags'].append(0)
+                flow['tcp_win'].append(0)
+                flow['is_tcp'].append(False)
 
-    def get_flows_features(self):
-        print('Started extracting ground truth labels for flows...')
-        self._apps = self._get_flow_labels()
-        print('Got {} unique flows!'.format(len(self._apps)))
+            app = apps[connection].split('.')
+            flow['proto'].append(app[0])
+            flow['subproto'].append(app[1] if len(app) > 1 else '')
 
-        flow_counter = 0
-        print('Started extracting features of packets...')
-        raw_flows = self._get_raw_flows()
-        for key in raw_flows:
-            raw_df = _get_raw_flow_df(raw_flows[key])
-            # format with a nice key
-            key = '{} {}:{} {}:{}'.format(key[0].upper(),
-                                          *list(key[1])[0],
-                                          *list(key[1])[1])
+    return flows
 
-            self._flows.update({key: _extract_feature_stats(raw_df)})
-            flow_counter += 1
-            if flow_counter % 100 == 0:
-                print('Processed {} flows...'.format(flow_counter))
 
-        self.flow_features = pd.DataFrame(self._flows).T
-        return self.flow_features
+def _format_connection(connection: Connection) -> str:
+    peers = list(connection.peers)
+    return '{} {}:{} {}:{}'.format(connection.proto.upper(),
+                                   peers[0].address, peers[0].port,
+                                   peers[1].address, peers[1].port)
 
-    def save_to_file(self):
-        pure_filename = _pure_filename(self.traffic_filename)
-        self.csv_filename = os.path.join(
-            self._config['offline']['csv_folder'],
-            'flows_{}split_{}.csv'.format(
-                self.strip,
-                pure_filename)
-        )
-        print('Saving features to {}...'.format(self.csv_filename))
-        self.flow_features.to_csv(self.csv_filename, index=True, sep='|')
+
+def _get_flows_features(ndpi_filename: str,
+                        traffic_filename: str,
+                        max_packets_per_flow: typing.Optional[int] = None) -> pd.DataFrame:
+    logging.info('Started extracting ground truth labels for flows...')
+    apps = _get_flow_apps(ndpi_filename, traffic_filename)
+    logging.info('Got {} unique flows!'.format(len(apps)))
+    flows = {}
+    logging.info('Started extracting features of packets...')
+    raw_flows = _get_raw_flows(apps, traffic_filename, max_packets_per_flow=max_packets_per_flow)
+    for flow_counter, (connection, flow) in enumerate(raw_flows.items()):
+        key = _format_connection(connection)
+        sorted_df = _raw_flow_to_sorted_dataframe(flow)
+        stats = _extract_rawflow_features(sorted_df)
+        flows[key] = stats
+        if flow_counter % 100 == 0:
+            logging.info(f'Processed {flow_counter} flows...')
+    else:
+        logging.info(f'Processed {len(raw_flows)} flows total')
+    return pd.DataFrame(flows).T
 
 
 def main():
@@ -321,11 +321,23 @@ def main():
 
     config = configparser.ConfigParser()
     config.read(args.config)
+    max_packets_per_flow = int(config['parser']['packetLimitPerFlow'])
     pcap_files = args.pcapfiles or [config['parser']['PCAPfilename']]
     for pcap_file in pcap_files:
-        parsed_pcap = PCAPParser(config, pcap_file)
-        parsed_pcap.get_flows_features()
-        parsed_pcap.save_to_file()
+        features = _get_flows_features(
+            config['parser']['nDPIfilename'],
+            pcap_file,
+            max_packets_per_flow=max_packets_per_flow
+        )
+        pure_filename = _pure_filename(pcap_file)
+        csv_filename = os.path.join(
+            config['offline']['csv_folder'],
+            'flows_{}split_{}.csv'.format(
+                max_packets_per_flow,
+                pure_filename)
+        )
+        logging.info('Saving features to {}...'.format(csv_filename))
+        features.to_csv(csv_filename, index=True, sep='|')
 
 
 if __name__ == "__main__":
