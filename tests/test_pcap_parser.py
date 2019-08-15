ef324ead7ee0ee683d14c1b835a48ad2b274aab1
eugene-katsevman <eugene.katsevman@gmail.com>
2019-08-15 22:55:55 +0300

    Feature/separate tests (#10)
    * move tests to separate folder
    
    * add filename and static fixtures

--- tests/test_pcap_parser.py
+++ tests/test_pcap_parser.py
@@ -16,27 +16,26 @@ def test_outer_functions():
     assert result == bytes([127, 0, 0, 1])
 
 
-def test_similar_ndpi_parse():
-    with open('ndpi_test_output.txt') as file:
+def test_similar_ndpi_parse(static):
+    with open(static('ndpi_test_output.txt')) as file:
         raw = file.read()
         result1 = pcapparser._parse_ndpi_output(raw)
         result2 = pcapparser._parse_ndpi_output(raw)
         assert result1 == result2
-        apps = result1
 
 
-def test_similar_flows():
-    filename = 'pcap_files/example.pcap'
-    flows1 = pcapparser._get_labeled_flows('bin/ndpiReader_deb', filename, max_packets_per_flow=100)
-    flows2 = pcapparser._get_labeled_flows('bin/ndpiReader_deb', filename, max_packets_per_flow=100)
+def test_similar_flows(filename):
+    example = filename('pcap_files/example.pcap')
+    ndpi = filename('bin/ndpiReader_deb')
+    flows1 = pcapparser._get_labeled_flows(ndpi, example, max_packets_per_flow=100)
+    flows2 = pcapparser._get_labeled_flows(ndpi, example, max_packets_per_flow=100)
     assert flows1 == flows2
 
 
-def test_similar_features():
-    filename = 'pcap_files/example.pcap'
-    flows = pcapparser._get_labeled_flows('bin/ndpiReader_deb', filename, max_packets_per_flow=100)
+def test_similar_features(filename):
+    example = filename('pcap_files/example.pcap')
+    ndpi = filename('bin/ndpiReader_deb')
+    flows = pcapparser._get_labeled_flows(ndpi, example, max_packets_per_flow=100)
     features = pcapparser._get_flows_features(flows)
     features2 = pcapparser._get_flows_features(flows)
     assert features.equals(features2)
-    features.to_csv('result1.csv')
-    features2.to_csv('result2.csv')
