5644d75c4f908745fd3db0f724c946b8f89807a8
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-05-26 23:23:46 +0300

    add processing script

---
+++ csv_files/process.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ls -1d ~/Applications/traffic_dumps/separated_iot_devices/* | xargs -n1 -t python ../flow_parser.py -p
+python ../flow_parser.py -p ~/Applications/traffic_dumps/non_iot.pcap
