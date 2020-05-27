4058b42f3027057d48953d4887c06a33d08ec0d2
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-05-28 01:11:28 +0300

    feature_prefixes in sep class | rm unused conftest

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,8 @@
-from .formatter import read_dataset, prepare_data, save_dataset
+from .formatter import read_dataset, prepare_data, save_dataset, TARGET_CLASS_COLUMN
 
 __all__ = [
     read_dataset,
     save_dataset,
     prepare_data,
+    TARGET_CLASS_COLUMN
 ]
