1bf92c451eb1a3df4c977ed1cd4f3ac642b1e0f6
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-06-05 18:30:43 +0300

    add raw features option to parser

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,27 @@ import pandas as pd
 
 import settings
 
-STATIC_DIR = settings.BASE_DIR / 'tests' / 'static'
-
 
 @pytest.fixture
 def dataset():
-    return pd.read_csv(STATIC_DIR / 'example_20packets.csv', na_filter='')
+    return pd.read_csv(settings.TEST_STATIC_DIR / 'example_20packets.csv', na_filter='')
+
+
+@pytest.fixture
+def raw_dataset():
+    return pd.read_csv(settings.TEST_STATIC_DIR / 'raw_example_20packets.csv', na_filter='')
+
+
+@pytest.fixture
+def classif_config():
+    return {'SVM': {'type': 'OneVsOneClassifier',
+                    'params': {'estimator': {'type': 'LinearSVC',
+                                             'params': {'tol': 1e-05}}, 'n_jobs': -1},
+                    'param_search_space': {'estimator__C': [0.1, 1, 10], 'estimator__loss': ['squared_hinge'],
+                                           'estimator__dual': [True, False]}},
+            'DecTree': {'type': 'DecisionTreeClassifier',
+                        'param_search_space': {'max_depth': [6, 9, 12, 15, 18], 'max_features': [10, 20, 30],
+                                               'criterion': ['entropy']}},
+            'GradBoost': {'type': 'GradientBoostingClassifier',
+                          'param_search_space': {'n_estimators': [50], 'max_depth': [2, 3, 4, 5],
+                                                 'learning_rate': [0.01, 0.05, 0.1]}}}
--- tests/test_pcap_parser.py
+++ tests/test_pcap_parser.py
@@ -6,10 +6,10 @@ import flow_parser
 import settings
 
 
-def test_feature_persistence(filename):
+def test_feature_persistence():
     pcap_filename = (settings.BASE_DIR / 'pcap_files/example.pcap').as_posix()
-    features = flow_parser.parse_to_dataframe(pcap_filename)
-    features2 = flow_parser.parse_to_dataframe(pcap_filename)
+    features = flow_parser.parse_features_to_dataframe(pcap_filename)
+    features2 = flow_parser.parse_features_to_dataframe(pcap_filename)
     assert features.equals(features2)
 
 
@@ -20,9 +20,18 @@ def test_parser_output(dataset):
         return x
 
     pcap_filename = (settings.BASE_DIR / 'pcap_files/example.pcap').as_posix()
-    parsed_features = flow_parser.parse_to_dataframe(pcap_filename)
+    parsed_features = flow_parser.parse_features_to_dataframe(pcap_filename)
     parsed_features = parsed_features.apply(_serialize, axis=1)
     pd.testing.assert_frame_equal(parsed_features, dataset,
+                                  check_less_precise=2,
+                                  check_like=True,
+                                  check_categorical=False)
+
+
+def test_raw_parser_output(raw_dataset):
+    pcap_filename = (settings.BASE_DIR / 'pcap_files/example.pcap').as_posix()
+    parsed_features = flow_parser.parse_features_to_dataframe(pcap_filename, raw_features=True)
+    pd.testing.assert_frame_equal(parsed_features, raw_dataset,
                                   check_less_precise=2,
                                   check_like=True,
                                   check_categorical=False)
