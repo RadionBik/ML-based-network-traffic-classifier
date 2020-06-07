524295f2fb1aae30fe01c8493641e97e47ed5b19
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-06-07 17:31:03 +0300

    parse with raw packets

--- csv_files/process.sh
+++ csv_files/process.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ls -1d ~/Applications/traffic_dumps/separated_iot_devices/* | xargs -n1 -t python ../flow_parser.py -p
-python ../flow_parser.py -p ~/Applications/traffic_dumps/non_iot.pcap
+ls -1d ~/Applications/traffic_dumps/separated_iot_devices/* | xargs -n1 -t python ../flow_parser.py --raw -p
+python ../flow_parser.py -p ~/Applications/traffic_dumps/non_iot.pcap --raw
