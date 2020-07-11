f85d2cb8f82667e4908b3a4a8782e189c81de595
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-07-12 00:12:38 +0300

    set timeouts depending on mode

--- settings.py
+++ settings.py
@@ -24,18 +24,15 @@ PCAP_OUTPUT_DIR = BASE_DIR / 'csv_files'
 IP_PROTO_MAPPING = _read_protocol_mapping()
 RANDOM_SEED = 1
 
-# set via analysis of packet number distribution within sessions @ mawi.wide.ad.jp/mawi/ditl/ditl2020/ pcaps,
-# such that the limit is close to .99 percentile
-
-PACKET_LIMIT_PER_FLOW = int(os.getenv('PACKET_LIMIT_PER_FLOW', 128))
+PACKET_LIMIT_PER_FLOW = int(os.getenv('PACKET_LIMIT_PER_FLOW', 20))
 LOWER_BOUND_CLASS_OCCURRENCE = int(os.getenv('LOWER_BOUND_CLASS_OCCURRENCE', 10))
 
 # customize, if needed
 TARGET_CLASS_COLUMN = 'target_class'
 
 # nfstream params
-# the idle timeout follows many papers on traffic identification
+# the idle timeout follows many papers on traffic identification (JOY has 10 sec)
 IDLE_TIMEOUT = 60
-# active timeouts are set similarly to Cisco's JOY tool
-ACTIVE_TIMEOUT_ONLINE = 30
-ACTIVE_TIMEOUT_OFFLINE = 30
+# active timeouts are set similarly, (Cisco's JOY tool has 30 sec)
+ACTIVE_TIMEOUT_ONLINE = 60
+ACTIVE_TIMEOUT_OFFLINE = 10e5
