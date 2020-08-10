fix attention masking

pretraining/dataset.py
@@ -58,7 +58,7 @@ class FlowIterDataset(IterableDataset):
 
 
 class FlowDataset(Dataset):
-    def __init__(self, tokenizer: PacketTokenizer, folder_path: str, train_mode=True):
+    def __init__(self, tokenizer: PacketTokenizer, folder_path: str):
         assert os.path.isdir(folder_path)
         # TODO feature caching, multiple workers?, filter out one-packet flows
 
@@ -66,7 +66,6 @@ class FlowDataset(Dataset):
         logger.info("initializing dataset from %s with %s files", folder_path, len(self.source_files))
 
         self.tokenizer = tokenizer
-        self.train_mode = train_mode
 
         raw_flows = pd.concat((pd.read_csv(csv, usecols=self.tokenizer.packet_quantizer.raw_columns, dtype=np.float32)
                                for csv in self.source_files), ignore_index=True)
@@ -91,7 +90,7 @@ class FlowDataset(Dataset):
 @dataclass
 class FlowCollator:
     """
-    Data collator used for language modeling.
+    Data collator used for traffic flow modeling.
     - collates batches of tensors, honoring their tokenizer's pad_token
     - preprocesses batches for masked language modeling
     """

pretraining/tokenizer.py
@@ -91,7 +91,7 @@ class PacketTokenizer(PreTrainedTokenizerBase):
             flows = flows.values
 
         if flows.shape[1]//2 != self.max_model_input_sizes:
-            logger.warning(f'input number of features ({flows.shape[1]//2}) does not match '
+            logger.debug(f'input number of features ({flows.shape[1]//2}) does not match '
                            f'max_model_input_sizes ({self.max_model_input_sizes})')
         clusters = self.packet_quantizer.transform(flows)
 
@@ -101,7 +101,7 @@ class PacketTokenizer(PreTrainedTokenizerBase):
         result = {'input_ids': clusters.astype(np.int64)}
 
         if return_attention_mask:
-            token_mask = (clusters != self.packet_quantizer.non_packet_value).astype(np.int64)
+            token_mask = (clusters != self.pad_token_id).astype(np.int64)
             result.update({'attention_mask': token_mask})
 
         return BatchEncoding(result, tensor_type=TensorType(return_tensors), prepend_batch_axis=False)