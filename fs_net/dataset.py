40a1aeef0201f368dd4418280198ec77d0ac160b
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2020-10-20 23:25:33 +0300

    rm special tokens except for padding, fix bug

--- fs_net/dataset.py
+++ fs_net/dataset.py
@@ -9,7 +9,7 @@ from gpt_model.classifier.dataset import ClassificationQuantizedDataset
 class ClassificationQuantizedDatasetNoAttention(ClassificationQuantizedDataset):
     def __getitem__(self, i: int) -> Tuple[torch.Tensor, torch.Tensor]:
         enc_flow = self.tokenizer.batch_encode_packets(self.raw_flows[i].reshape(1, -1).astype(np.float64),
-                                                       add_special_tokens=True,
+                                                       add_special_tokens=False,
                                                        return_attention_mask=False).data
         X = enc_flow['input_ids']
         y = torch.as_tensor(self.targets[i], dtype=torch.long)
